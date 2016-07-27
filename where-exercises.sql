9ddb738b2e1ccfaca80875bfe1c3d0b592d032aa
TJ Becker <tj.becker@TJs-MacBook-Air.local>
2016-07-27 11:36:26 -0500

    Where exercises.  MySQL.

---
+++ where-exercises.sql
@@ -0,0 +1,8 @@
+SELECT * FROM employees WHERE (first_name = 'Irena'
+							OR first_name = 'Vidya'
+							OR first_name = 'Maya')
+							AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE ('E%');
+SELECT * FROM employees WHERE hire_date LIKE ('199%');
+SELECT * FROM employees WHERE birth_date LIKE ('%12-25');
+SELECT * FROM employees WHERE last_name LIKE ('%q%');
