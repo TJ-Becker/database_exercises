4f9caf63b012bb74a88a2acda0536e4017b32d86
TJ Becker <tj.becker@TJs-MacBook-Air.local>
2016-07-27 09:46:04 -0500

    delete sql exercises.

--- albums-seeder.sql
+++ albums-seeder.sql
@@ -1,8 +1,9 @@
 USE codeup_test_db;
+TRUNCATE albums;
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES
 ('Michael Jackson', 'Thriller', 1982, 45000000, 'Pop'),
 ('Nirvana', 'Nevermind', 1991, 19400000, 'Grunge'),
 ('AC/DC', 'Back in Black', 1980, 25900000, 'Hard Rock'),
-('Pink Floyd', 'The Dark Side of the Moon', 1973, 2270000, 'Progressive Rock'),
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 22700000, 'Progressive Rock'),
 ('The Beatles', 'Sgt. Pepper''s Lonely Island Club', 1967, 13100000, 'Rock');
---
+++ delete-exercises.sql
@@ -0,0 +1,3 @@
+SELECT * FROM albums WHERE release_date > 1985;
+SELECT * FROM albums WHERE genre = 'Rock';
+SELECT * FROM albums WHERE artist = 'The Beatles';
